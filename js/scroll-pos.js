818a5c96fedbdafc3172594c588547357cf5788b
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-12-11 22:18:40 -0600

    changed script loading order

--- js/scroll-pos.js
+++ js/scroll-pos.js
@@ -1,20 +1,22 @@
-var prevScrollPos = window.pageYOffset;
-    window.addEventListener("scroll", function showHeaderOnScroll() {
-        let profileHeaderElem = document.getElementById("profileHeader");
-        let currentScrollPos = window.pageYOffset;
-        let resetHeaderStyle = false;
-        let showNavBarOnScrollUp = true;
-        let showNavBar = showNavBarOnScrollUp ? prevScrollPos > currentScrollPos : currentScrollPos > 0;
-        if (showNavBar) {
-            profileHeaderElem.classList.add("showHeaderOnTop");
-        } else {
-            resetHeaderStyle = true;
-        }
-        if (currentScrollPos === 0) {
-            resetHeaderStyle = true;
-        }
-        if (resetHeaderStyle) {
-            profileHeaderElem.classList.remove("showHeaderOnTop");
-        }
-        prevScrollPos = currentScrollPos;
-    });
+let prevScrollPos = window.scrollY;
+window.addEventListener("scroll", function showHeaderOnScroll() {
+  let profileHeaderElem = document.getElementById("profileHeader");
+  let currentScrollPos = window.scrollY;
+  let resetHeaderStyle = false;
+  let showNavBarOnScrollUp = true;
+  let showNavBar = showNavBarOnScrollUp
+    ? prevScrollPos > currentScrollPos
+    : currentScrollPos > 0;
+  if (showNavBar) {
+    profileHeaderElem.classList.add("showHeaderOnTop");
+  } else {
+    resetHeaderStyle = true;
+  }
+  if (currentScrollPos === 0) {
+    resetHeaderStyle = true;
+  }
+  if (resetHeaderStyle) {
+    profileHeaderElem.classList.remove("showHeaderOnTop");
+  }
+  prevScrollPos = currentScrollPos;
+});
--- js/set-theme.js
+++ js/set-theme.js
@@ -1,5 +1,5 @@
-
 if (document.body.className.includes("light")) {
-        document.body.classList.add('dark');
-        localStorage.setItem("pref-theme", 'dark');
-    }
+  document.body.classList.add("dark");
+
+  localStorage.setItem("pref-theme", "dark");
+}
