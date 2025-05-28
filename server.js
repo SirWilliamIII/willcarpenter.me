98435990e7828fe214b4ce3a62ca9233e6a70ae4
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-05-28 16:17:55 -0500

    removed dead files, fixed GA

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files (JS, CSS, images, etc.)
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route: Home page
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// ✅ Route: /analytics
+app.get("/analytics", (_, res) => {
+  res.sendFile(path.join(__dirname, "public", "analytics.html"));
+});
+
+// Catch-all 404 handler
 app.use((_, res) => {
   res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
